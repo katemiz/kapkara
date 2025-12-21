6bd21407531073fdb31b08ac3485c4d2674a92ab
Kılıç Ali Temiz <katemiz@gmail.com>
2025-12-21 12:22:52 +0300

    Updated fun stuff menu

--- resources/js/Pages/FunStuff/Index.svelte
+++ resources/js/Pages/FunStuff/Index.svelte
@@ -5,35 +5,14 @@
 <Layout>
     <section class="section">
         <aside class="menu">
-            <p class="menu-label">General</p>
+            <p class="menu-label">Engineering</p>
             <ul class="menu-list">
                 <li>
                     <a href="/funstuff/gauss"
                         >Gauss Distribution with Galton Board</a
                     >
                 </li>
-                <li><a href="/">Customers</a></li>
-            </ul>
-            <p class="menu-label">Administration</p>
-            <ul class="menu-list">
-                <li><a href="/">Team Settings</a></li>
-                <li>
-                    <a href="/" class="is-active">Manage Your Team</a>
-                    <ul>
-                        <li><a href="/">Members</a></li>
-                        <li><a href="/">Plugins</a></li>
-                        <li><a href="/">Add a member</a></li>
-                    </ul>
-                </li>
-                <li><a href="/">Invitations</a></li>
-                <li><a href="/">Cloud Storage Environment Settings</a></li>
-                <li><a href="/">Authentication</a></li>
-            </ul>
-            <p class="menu-label">Transactions</p>
-            <ul class="menu-list">
-                <li><a href="/">Payments</a></li>
-                <li><a href="/">Transfers</a></li>
-                <li><a href="/">Balance</a></li>
+                <li><a href="/">Maxell's Demon (Coming)</a></li>
             </ul>
         </aside>
     </section>
